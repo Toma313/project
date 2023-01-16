0fe59ee14ddd352c43b57cc796d41f5ccc8e3712
Tamara <tamara@gmail.com>
2023-01-16 12:22:44 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const answers=[];
+// // answers[0]= prompt('what is your name?', '');
+// // answers[1]= prompt('what is your surname?', '');
+// // answers[2]= prompt('what is your age?', '');
+// //document.write(answers);
+// console.log(typeof(answers));
+
+const category= 'toys';
+console.log(' ')
